062d8d72abcf6645120e393dd38b418fce292280
Emery <iing7050737@gmail.com>
2025-02-25 00:32:20 +0800

    完成 hw week6

--- week5/app.js
+++ week5/app.js
@@ -9,6 +9,7 @@ const skillRouter = require('./routes/skill')
 const userRouter = require('./routes/user')
 const adminRouter = require('./routes/admin')
 const coachesRouter = require('./routes/coaches')
+const coursesRouter = require('./routes/courses')
 
 const app = express()
 app.use(cors())
@@ -31,9 +32,10 @@ app.get('/healthcheck', (req, res) => {
 })
 app.use('/api/credit-package', creditPackageRouter)
 app.use('/api/skill', skillRouter)
-app.use('/api/user', userRouter)
+app.use('/api/users', userRouter)
 app.use('/api/admin', adminRouter)
 app.use('/api/coaches', coachesRouter)
+app.use('/api/courses', coursesRouter)
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
